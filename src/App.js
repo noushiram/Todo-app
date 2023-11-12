e9473e99812dacf143fe38295158dd8943192e25
noushirap <noushi1617@gmail.com>
2023-11-12 09:41:39 +0530

    todo list items and checked

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import { useState } from 'react';
 function App() {
   const [toDos, setTodos] = useState([])
   const [toDo, setTodo] = useState('')
-  console.log(toDo)
-  console.log(toDos)
+  
   return (
     <div >
       <div className="app">
@@ -18,17 +17,26 @@ function App() {
       </div>
       <div className="input">
         <input type="text"  value={toDo} onChange={(e)=>setTodo(e.target.value)}placeholder="🖊️ Add item..." />
-        <i onClick={()=>setTodos([...toDos,toDo])}className="fas fa-plus" ></i>
+        <i onClick={()=>setTodos([...toDos,{id:Date.now(),text:toDo,status:false}])}className="fas fa-plus" ></i>
       </div>
       <div className="todos">
         {
-          toDos.map((value)=>{
+          toDos.map((obj)=>{
             return(
           
         <div className="todo">
           <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>{value}</p>
+            <input type="checkbox" onChange={(e)=>{
+            console.log(e.target.value)
+            console.log(obj)
+            setTodos(toDos.filter(obj2=>{
+              if(obj2.id===obj.id){
+                obj2.status=e.target.checked
+              }
+              return obj2
+              }))
+            } } value={obj.status}name="" id="" />
+            <p>{obj.text}</p>
           </div>
           <div className="right">
             <i className="fas fa-times"></i>
